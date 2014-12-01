86d5c9150006b1546f8ce1f6757c4e1559fbe774
Bhaskar <bhaskar@ec.is>
2014-12-01 07:18:39 +0530

    include mock service fetch image list

--- www/js/services.js
+++ www/js/services.js
@@ -15,10 +15,46 @@ angular.module('starter.services', [])
   }
 }])
 .factory('MyVideosService', ['$http', function($http) {
+  var baseimages = [{
+    "id": "cat1-1-1",
+    "title": "Titanic",
+    "images": [{"thumb-url": "../img/assets/1.jpg"}]
+  },
+  {
+    "id": "cat1-1-2",
+    "title": "Thor",
+    "images": [{"thumb-url": "../img/assets/2.jpg"}]
+  },
+  {
+    "id": "cat1-1-3",
+    "title": "Iron Man",
+    "images": [{"thumb-url": "../img/assets/3.jpg"}]
+  }];
+  var currentBuffer = [];
+  baseimages.forEach(function(item){
+    currentBuffer.push(item);
+  });
   return {
     getMyVideos: function () {
       //Returns all my videos      
-      return $http.get('data/myvideos.json');
+//      return $http.get('data/myvideos.json');
+//Mock Data
+  baseimages.forEach(function(item){
+    currentBuffer.push({
+      id:item.id,
+      title:item.title,
+      images:item.images
+    });
+  });
+
+  return {
+
+    then:function(f){
+      f({
+        data:currentBuffer
+        });
     }
+  };
   }
+}
 }]);
